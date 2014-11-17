22e8f833a17ca0d89ec78571d7642700e04c8d43
Rodrigo Pastro <rodrigo.pastro@unify.com>
2014-11-17 12:13:10 -0500

    Initial Commit

---
+++ prototypes/score/score/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  score
+//
+//  Created by Unify Inc on 11/17/14.
+//  Copyright (c) 2014 Unify Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
