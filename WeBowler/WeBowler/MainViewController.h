3763e0de736680936427dfbf2bd79b972353f1bb
Christian Garbin <garbin.christian@gmail.com>
2014-11-18 16:06:29 -0500

    Separate view controllers for the views

---
+++ WeBowler/WeBowler/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  WeBowler
+//
+//  Created by Christian Garbin on 11/18/14.
+//  Copyright (c) 2014 Unify Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
